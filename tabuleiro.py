111aca6425f88789510059607229e8cc20f6eef1
victorjtn <josev2096@gmail.com>
2025-07-10 19:36:45 -0300

    alteração tabuleiro

--- tabuleiro.py
+++ tabuleiro.py
@@ -12,4 +12,29 @@ class Tabuleiro:
        
         
     def tem_campeao(self):
+        # Verifica as colunas
+        for c in range(0,3):
+            soma = self.matriz[0][c] + self.matriz[1][c] + self.matriz[2][c]
+            if soma == 3:
+                return Tabuleiro.JOGADOR_0
+            elif soma == 12:
+                return Tabuleiro.JOGADOR_X
+        # Verifica as linhas
+        for l in range(0,3):
+            soma = self.matriz[l][0] + self.matriz[l][1] + self.matriz[l][2]
+            if soma == 3:
+                return Tabuleiro.JOGADOR_0
+            elif soma == 12:
+                return Tabuleiro.JOGADOR_X
+        # Verifica as diagonais
+        soma = self.matriz[0][0] + self.matriz[1][1] + self.matriz[2][2]
+        if soma == 3:
+            return Tabuleiro.JOGADOR_0
+        elif soma == 12:
+            return Tabuleiro.JOGADOR_X
+        soma = self.matriz[0][2] + self.matriz[1][1] + self.matriz[2][0]
+        if soma == 3:
+            return Tabuleiro.JOGADOR_0
+        elif soma == 12:
+            return Tabuleiro.JOGADOR_X
         return Tabuleiro.DESCONHECIDO
