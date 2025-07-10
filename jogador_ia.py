e9e6bb607d587137dccfdd290e84fd47bbd1f5de
victorjtn <josev2096@gmail.com>
2025-07-10 19:17:58 -0300

    jogo_velha

---
+++ jogador_ia.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from random import randint
+
+from jogador import Jogador
+from tabuleiro import Tabuleiro
+
+class JogadorIA(Jogador):
+    def __init__(self, tabuleiro : Tabuleiro, tipo : int):
+        super().__init__(tabuleiro, tipo)
+            
+
+    def getJogada(self) -> (int, int):
+        lista = []
+        for l in range(0,3):
+            for c in range(0,3):
+                if self.matriz[l][c] == Tabuleiro.DESCONHECIDO:
+                    lista.append((l, c))
+                    
+        if(len(lista) > 0):
+            p = randint(0, len(lista)-1)
+            return lista[p]
+        else:
+           return None
---
+++ tabuleiro.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Tabuleiro:
+    DESCONHECIDO = 0
+    JOGADOR_0 = 1
+    JOGADOR_X = 4
+
+    def __init__(self):
+        self.matriz = [ [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO], 
+                        [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO],
+                        [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO]]
+       
+        
+    def tem_campeao(self):
+        return Tabuleiro.DESCONHECIDO
